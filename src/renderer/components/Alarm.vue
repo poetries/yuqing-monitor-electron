64c2f27a8796fa66c16b546ca5d6a9af3495bb6a
poetries <jingguanliuye@gmail.com>
2019-01-21 17:37:15 +0800

    update

--- src/renderer/components/Alarm.vue
+++ src/renderer/components/Alarm.vue
@@ -1,32 +1,97 @@
 <template>
   <div id="alarm"  class="content">
+   
         <div class="input_list">
             <ul>
-                <li> <span>报警提醒电话：</span><el-input v-model="tel" placeholder="请输入内容"></el-input></li>
-                <li> <span>报警提醒邮箱：</span><el-input v-model="email" placeholder="请输入内容"></el-input></li>                    
+                <li> <span>报警提醒电话：</span><el-input v-model="alarm.tel" placeholder="请输入内容"></el-input></li>
+                <li> <span>报警提醒邮箱：</span><el-input v-model="alarm.email" placeholder="请输入内容"></el-input></li>                    
             </ul>
-             <el-button type="primary" class="save">保存</el-button>
+
+             <el-button type="primary" class="save" @click="saveAlarm()">保存</el-button>
         </div>
   </div>
 </template>
+
 <script>
+	import tools from '../model/tools.js';
+
     export default {
       name: 'alarm',
+
       data(){
           return {
-            tel:"124214124",
-            email:"124214124"
+                alarm:{}
           }
       },
       methods: {
-        open (link) {
-          // this.$electron.shell.openExternal(link)
+        getAlarmList () {
+              var userinfo=tools.storage.get('userinfo');
+              var sign=tools.sign({
+                'a':'alarmList',
+                'uid':userinfo.id,
+                'salt':userinfo.salt 
+              })              
+              var api=tools.config.apiUrl+'index.php?m=Api&a=alarmList&uid='+userinfo.id+'&sign='+sign;
+           
+              this.$http.get(api)
+                .then( (response)=> {
+                    this.alarm=response.data.result;
+                })
+                .catch( (error) =>{
+                     this.$message({
+                        message: error,
+                        type: 'warning'
+                      });
+                });
+        },
+        saveAlarm(){
+             var userinfo=tools.storage.get('userinfo');
+              var sign=tools.sign({
+                'a':'editAlarm',
+                'uid':userinfo.id,
+                'salt':userinfo.salt   
+              })              
+              // 建议正式项目中签名所有的字段 --- 更安全   
+              
+              // 执行提交数据
+              this.$http.post(tools.config.apiUrl+'index.php?m=Api&a=editAlarm', {                  
+                    tel:this.alarm.tel,
+                    email:this.alarm.email,
+                    uid:userinfo.id,                    
+                    sign:sign   
+                })
+                .then( (response)=> {
+                     console.log(response);
+
+                    if(response.data.success){
+                     this.$message({
+                        message: '设置成功',
+                        type: 'success'
+                      });
+                    }else{
+                        this.$message({
+                            message: '设置失败',
+                            type: 'success'
+                        });
+                    }
+                })
+                .catch((error)=>{
+                     this.$message({
+                        message: error,
+                        type: 'warning'
+                      });
+                });
         }
+      },
+      mounted() {
+          //获取舆情报警信息
+          this.getAlarmList();   
       }
     }
 </script>
 
 <style lang="scss">
+
  #alarm{
         border:1px solid #eee;
         padding: 10px;
--- src/renderer/components/KeyWord.vue
+++ src/renderer/components/KeyWord.vue
@@ -1,98 +1,153 @@
 <template>
   <div id="keywords"  class="content">
+
+
+ <!-- 增加关键词 -->
         <div class="main_top">
-            <el-button type="primary" @click="dialogFormVisible = true">增加关键词</el-button>
+            <el-button type="primary" @click="dialogFormVisible = true">增加关键词</el-button>            
+
             <el-dialog title="增加关键词" :visible.sync="dialogFormVisible">
-              <el-form>      
+              <el-form>
                      <el-form-item label="必须包含关键词" label-width="120px">
-                        <el-input v-model="form.keyword" auto-complete="off"></el-input>
+                        <el-input v-model="addForm.keyword" auto-complete="off"></el-input>
                     </el-form-item>
+
                      <el-form-item label="可包含关键词" label-width="120px">
-                        <el-input v-model="form.may_keyword" auto-complete="off"></el-input>
+                        <el-input v-model="addForm.may_keyword" auto-complete="off"></el-input>
                     </el-form-item>
                      <el-form-item label="不包含关键词" label-width="120px">
-                        <el-input v-model="form.nokeyword" auto-complete="off"></el-input>
+                        <el-input v-model="addForm.nokeyword" auto-complete="off"></el-input>
                     </el-form-item>
+
                      <el-form-item label="监控评率" label-width="120px">
-                        <el-input v-model="form.frequency" auto-complete="off"></el-input>
+                        <el-input v-model="addForm.frequency" auto-complete="off"></el-input>
                     </el-form-item>
+                
               </el-form>
               <div slot="footer" class="dialog-footer">
                 <el-button @click="dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary" @click="doPostKeywords()">确 定</el-button>
+                <el-button type="primary" @click="doAddKeywords()">确 定</el-button>
               </div>
             </el-dialog>
         </div>
+
+
+<!-- 增加关键词完成 -->
+
+
+<!-- 修改关键词开始 -->
+      <el-dialog title="修改关键词" :visible.sync="eidtDialogVisible">
+          <el-form>
+                  <el-form-item label="必须包含关键词" label-width="120px">
+                    <el-input v-model="editForm.keyword" auto-complete="off"></el-input>
+                </el-form-item>
+
+                  <el-form-item label="可包含关键词" label-width="120px">
+                    <el-input v-model="editForm.may_keyword" auto-complete="off"></el-input>
+                </el-form-item>
+                  <el-form-item label="不包含关键词" label-width="120px">
+                    <el-input v-model="editForm.nokeyword" auto-complete="off"></el-input>
+                </el-form-item>
+
+                  <el-form-item label="监控评率" label-width="120px">
+                    <el-input v-model="editForm.frequency" auto-complete="off"></el-input>
+                </el-form-item>
+            
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="closeModal()">取 消</el-button>
+            <el-button type="primary" @click="doEditKeywords()">确 定</el-button>
+          </div>
+        </el-dialog>
+
+      <!-- 修改关键词完成 -->
+
+      <!-- 关键词列表开始 -->
+
         <table class="electron-table">
             <tr>
-                <th>标题/地址</th>
+               
 			        	<th>必须包含词</th>
                 <th>可包含词</th>
 				        <th>不包含词</th>            
-                <th width="60">监控频率</th>	
-                <th>负面数</th>	
-                <th>舆情总数</th>
-                <th>下次更新时间</th>
+                <th width="60">监控频率</th>	             
                 <th width="100">操作</th>	
             </tr>
-            <tr>
-        	    <td align="center"><a class="blue" href="/yuqing/admin.php?m=Keywords&amp;a=index&amp;cate_id=5">企业舆情</a></td>           
-              <td align="center">天然气分公司扁平化管理</td>
-              <td align="center"></td>      
-                      <td align="center"></td>
+            <tr v-for="item in list" :key="item.id">
+        	    
               <!--<td align="center"></td>-->
-              <td align="center">0 分</td>
-               <td align="center"><em style="color:red;"><a class="blue" href="/yuqing/admin.php?m=KeywordsSearch&amp;a=index&amp;id=26&amp;type=-1">负面：0</a></em>　</td>
+              <td align="center">{{item.keyword}}</td>
+              <td align="center">{{item.may_keyword}}</td>              
               <td align="center">
-                <a class="blue" href="/yuqing/admin.php?m=KeywordsSearch&amp;a=index&amp;id=26">1</a>	
+               {{item.nokeyword}}
+              </td>				   
+              <td align="center">
+                  {{item.frequency}}
+                
+                </td>             
+              <td align="center">
+               
+                <span class="blue"  @click="editKeywords(item.id)">编辑</span>　　
+                
+                　
+
+               <span class="blue"  @click="removeKeywords(item.id)">删除</span>　
+
+                
               </td>
-              <td align="center">2014-07-08 08:38:45</td>             
-              <td align="center">
-                <a class="blue" href="/yuqing/admin.php?m=KeywordsSearch&amp;a=index&amp;id=26">查看内容</a>　
-                <a class="blue" href="javascript:edit(26,'')">编辑</a>
-              </td>  
-            </tr>
+            </tr>           
+
         </table>
-        <div class="page">  
-            <el-pagination
-            background
-            layout="prev, pager, next"
-            :total="1000" @current-change="pageChange($event)">
-            </el-pagination>
-        </div>
+
+  <!-- 关键词列表完成 -->
   </div>
 </template>
+
 <script>
 
 	//引入tools模块
+
 	  import tools from '../model/tools.js';
+
     export default {
-      name: 'keyword',
+      name: 'keywords',
+
       data(){
+
         return{
+
           dialogFormVisible:false,
-          form:{}
+          eidtDialogVisible:false,/*编辑的数据*/
+          addForm:{},
+          editForm:{},   /*修改的数据*/
+          list:[]
+        
         }
       },
+      
       methods: {      
-        doPostKeywords(){
+        closeModal() {
+           this.dialogFormVisible=false;
+        },
+        doAddKeywords(){
+
+            // console.log(this.form.keyword);
 
               var userinfo=tools.storage.get('userinfo');
 
               var sign=tools.sign({
                 'a':'addKeywords',
                 'uid':userinfo.id,
-                'salt':userinfo.salt   //私钥
-              })
-              
-              //签名所有的字段  （支付之类的项目  建议签名所有的参数）
+                'salt':userinfo.salt   //私钥       不要传到url中
+              })              
+              // 建议正式项目中签名所有的字段 --- 更安全   
               
               // 执行提交数据
               this.$http.post(tools.config.apiUrl+'index.php?m=Api&a=addKeywords', {                  
-                    keyword:this.form.keyword,
-                    may_keyword:this.form.may_keyword,
-                    nokeyword:this.form.nokeyword,
-                    frequency:this.form.frequency,
+                    keyword:this.addForm.keyword,
+                    may_keyword:this.addForm.may_keyword,
+                    nokeyword:this.addForm.nokeyword,
+                    frequency:this.addForm.frequency,
                     uid:userinfo.id,                    
                     sign:sign   //签名
                 })
@@ -102,6 +157,9 @@
                     if(response.data.success){
 
                       this.dialogFormVisible=false;
+
+                      //增加完成重新获取数据
+                      this.getKeywordsList();
                     }else{
 
                       this.$message({
@@ -110,17 +168,171 @@
                       });
                     }
                 })
-                .catch(function (error) {
+                .catch((error)=>{
+                     this.$message({
+                        message: error,
+                        type: 'warning'
+                      });
+                });
+        },
+
+        getKeywordsList(){
+
+              var userinfo=tools.storage.get('userinfo');
+
+              var sign=tools.sign({
+                'a':'keywordsList',
+                'uid':userinfo.id,
+                'salt':userinfo.salt   //私钥       不要传到url中
+              })              
+              var api=tools.config.apiUrl+'index.php?m=Api&a=keywordsList&uid='+userinfo.id+'&sign='+sign;
+           
+              this.$http.get(api)
+                .then( (response)=> {
+                    // console.log(response);
+
+                    this.list=response.data.result;
+
+                })
+                .catch( (error) =>{
+                     this.$message({
+                        message: error,
+                        type: 'warning'
+                      });
+                });
+
+
+        },
+        editKeywords(id){
+            // alert(id);
+
+             this.eidtDialogVisible=true;
+
+
+            //请求api接口获取要编辑的数据
+           
+              var userinfo=tools.storage.get('userinfo');
+              var sign=tools.sign({
+                'id':id,
+                'a':'oneKeywordsList',
+                'uid':userinfo.id,
+                'salt':userinfo.salt   //私钥       不要传到url中
+              })              
+              var api=tools.config.apiUrl+'index.php?m=Api&a=oneKeywordsList&id='+id+'&uid='+userinfo.id+'&sign='+sign;
+           
+              this.$http.get(api)
+                .then( (response)=> {
+                    // console.log(response);
+
+                    this.editForm=response.data.result;
+
+                })
+                .catch( (error) =>{
+                     this.$message({
+                        message: error,
+                        type: 'warning'
+                      });
+                });
+
+          
+        }, 
+        //执行编辑操作
+        doEditKeywords(){
+
+              console.log(this.editForm);
+
+
+              var userinfo=tools.storage.get('userinfo');
+
+              var sign=tools.sign({
+                'id':this.editForm.id,
+                'a':'editKeywords',
+                'uid':userinfo.id,
+                'salt':userinfo.salt   //私钥       不要传到url中
+              })              
+             
+              // 执行提交数据
+              this.$http.post(tools.config.apiUrl+'index.php?m=Api&a=editKeywords', {   
+                    id:this.editForm.id,              
+                    keyword:this.editForm.keyword,
+                    may_keyword:this.editForm.may_keyword,
+                    nokeyword:this.editForm.nokeyword,
+                    frequency:this.editForm.frequency,
+                    uid:userinfo.id,                    
+                    sign:sign   //签名
+                })
+                .then( (response)=> {
+                    console.log(response);
+
+                    if(response.data.success){
+
+                      this.eidtDialogVisible=false;
+                      //增加完成重新获取数据
+                      this.getKeywordsList();
+                    }else{
+
+                      this.$message({
+                        message: response.data.message,
+                        type: 'warning'
+                      });
+                    }
+                })
+                .catch((error)=>{
+                     this.$message({
+                        message: error,
+                        type: 'warning'
+                      });
+                });
+
+
+        },
+        removeKeywords(id){
+
+              var userinfo=tools.storage.get('userinfo');
+
+              var sign=tools.sign({
+                'id':id,
+                'a':'removeKeywords',
+                'uid':userinfo.id,
+                'salt':userinfo.salt   //私钥       不要传到url中
+              })    
+             
+              this.$http.post(tools.config.apiUrl+'index.php?m=Api&a=removeKeywords', {   
+                    id:id,  
+                    uid:userinfo.id,                    
+                    sign:sign   //签名
+                })
+                .then( (response)=> {
+                    console.log(response);
+                    if(response.data.success){
+                      //增加完成重新获取数据
+                      this.getKeywordsList();
+                    }else{
+
+                      this.$message({
+                        message: response.data.message,
+                        type: 'warning'
+                      });
+                    }
+                })
+                .catch((error)=>{
                      this.$message({
                         message: error,
                         type: 'warning'
                       });
                 });
         }
+      },
+      mounted() {
+        this.getKeywordsList();
       }
     }
 </script>
 
 <style lang="scss">
- 
+ .blue{
+
+   color: blue;
+   cursor: pointer;
+ }
 </style>
